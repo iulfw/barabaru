18203e15baba65d194b593faa80a76eb411f7d52
iulfw <luisfernandowilliam68@gmail.com>
2024-06-13 21:43:24 +0700

    Fix Content Can't Load

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//library
-var flash   = require('express-flash');
-var session = require('express-session');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var menuRouter = require('./routes/menu'); // <-- route menu
+var menuRouter = require('./routes/menu');
 
 var app = express();
 
-// view engine setup
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,21 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ 
-  cookie: { 
-    maxAge: 60000 
-  },
-  store: new session.MemoryStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: 'secret'
-}))
-
-app.use(flash())
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/menu', menuRouter); // use route menu di Express
+app.use('/menu', menuRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* Read Previous File */
+/* Read Main Website */
 
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Home' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+    res.render('about', { title: 'About' });
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menus', { title: 'Menu' });
+    res.render('menus', { title: 'Menu' });
 });
 
 router.get('/feedback', function(req, res, next) {
-  res.render('feedback', { title: 'Feedback' });
+    res.render('feedback', { title: 'Feedback' });
 });
 
 module.exports = router;
